c9112a1f00298f37e9c34b7335834a10bcb065db
Ranie Jade Ramiso <raniejaderamiso@gmail.com>
2018-03-30 17:43:17 +1100

    Rename entities folder to components

--- engine/src/convention.rs
+++ engine/src/convention.rs
@@ -2,18 +2,18 @@ use std::path::{Path, PathBuf};
 
 #[derive(Debug)]
 pub struct Convention {
-    pub entities_dir: PathBuf,
+    pub components_dir: PathBuf,
     pub processors_dir: PathBuf,
     pub scripts_dir: PathBuf
 }
 
 impl Convention {
     pub fn create(root: &PathBuf) -> Convention {
-        let entities_dir = root.join(Path::new("entities"));
+        let components_dir = root.join(Path::new("components"));
         let processors_dir = root.join(Path::new("processors"));
         let scripts_dir = root.join(Path::new("scripts"));
         return Convention {
-            entities_dir,
+            components_dir,
             processors_dir,
             scripts_dir
         };
