b7c59c106df44f055d90ee8197f4e8ed7838bcfe
Ranie Jade Ramiso <raniejaderamiso@gmail.com>
2018-03-30 18:39:23 +1100

    Add sample lua files

---
+++ resources/components/vector.lua
@@ -0,0 +1,10 @@
+local Vector = {x: 0.0, y: 0.0}
+
+function Vector:new(object) {
+  object = object or {}
+  self.__index = self
+  setmetatable(object, self)
+  return self
+}
+
+return Vector
---
+++ resources/processors/simple.lua
@@ -0,0 +1,10 @@
+local Vector = Engine:require("components/vector")
+
+local Processor = { 
+  components = [Vector]
+}
+
+function Processor:process(elapsedTime, entities) {
+}
+
+return Processor
