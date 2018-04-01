33249288bc3ef546f549b89913af622b84ca3ea4
Ranie Jade Ramiso <raniejaderamiso@gmail.com>
2018-04-01 17:03:14 +1000

    Rework class registry return types

--- engine/src/api/lua/registry.rs
+++ engine/src/api/lua/registry.rs
@@ -5,29 +5,25 @@ static REGISTRY_NAME: &str = "__yamhc__registry";
 pub struct ClassRegistry<'lua>(&'lua Lua);
 
 impl<'lua> ClassRegistry<'lua> {
-    pub fn create(lua: &'lua Lua) -> ClassRegistry<'lua> {
-        let internal_registry = lua.create_table().unwrap();
-        lua.globals().set(REGISTRY_NAME, internal_registry).unwrap();
-        return ClassRegistry(lua);
+    pub fn create(lua: &'lua Lua) -> Result<ClassRegistry<'lua>> {
+        let internal_registry = lua.create_table()?;
+        lua.globals().set(REGISTRY_NAME, internal_registry)?;
+        return Ok(ClassRegistry(lua));
     }
 
-    pub fn get(&self, name: &String) -> Option<Table> {
-        let internal_registry = self.get_internal_registry();
-        let result = internal_registry.get::<_, Table>(name.to_owned());
-        return match result {
-            Ok(table) => Some(table),
-            Err(e) => None
-        };
+    pub fn get(&self, name: &String) -> Result<Table> {
+        let internal_registry = self.get_internal_registry()?;
+        return internal_registry.get::<_, Table>(name.to_owned())
     }
 
     pub fn register(&self, name: &String, class: Table<'lua>) -> Result<()> {
-        self.get_internal_registry()
+        self.get_internal_registry()?
             .set(name.to_owned(), class)?;
         return Ok(());
     }
 
-    fn get_internal_registry(&self) -> Table {
-        return self.0.globals().get::<_, Table>(REGISTRY_NAME).unwrap();
+    fn get_internal_registry(&self) -> Result<Table> {
+        return Ok(self.0.globals().get::<_, Table>(REGISTRY_NAME)?);
     }
 }
 
@@ -37,23 +33,23 @@ mod tests {
     #[test]
     fn test_register() {
         let lua = Lua::new();
-        let registry = ClassRegistry::create(&lua);
+        let registry = ClassRegistry::create(&lua).unwrap();
 
         let name = String::from("MyClass");
         let class = lua.create_table().unwrap();
 
         registry.register(&name, class);
 
-        assert!(registry.get(&name).is_some());
+        assert!(registry.get(&name).is_ok());
     }
 
     #[test]
     fn test_get_none() {
         let lua = Lua::new();
-        let registry = ClassRegistry::create(&lua);
+        let registry = ClassRegistry::create(&lua).unwrap();
 
         let name = String::from("MyClass");
 
-        assert!(registry.get(&name).is_none());
+        assert!(registry.get(&name).is_err());
     }
 }
