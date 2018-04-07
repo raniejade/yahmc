de83c3fc3066b81ecd09fdeb8d1b9bfb2a45cfb8
Ranie Jade Ramiso <raniejaderamiso@gmail.com>
2018-04-07 22:17:09 +1000

    More entitiy stuff

--- ecs/src/entity.rs
+++ ecs/src/entity.rs
@@ -4,18 +4,20 @@ use std::any::TypeId;
 
 use super::component::{Component, ComponentId};
 
-pub struct Entity<F: Fn(TypeId) -> ComponentId> {
+pub struct Entity<'a, F: Fn(TypeId) -> ComponentId> {
     mask: BitSet,
     components: FxHashMap<ComponentId, Box<Component>>,
-    component_mapper: F
+    component_mapper: F,
+    mark_dirty: &'a mut FnMut()
 }
 
-impl<F: Fn(TypeId) -> ComponentId> Entity<F> {
-    pub(crate) fn new(component_mapper: F) -> Self {
+impl<'a, F: Fn(TypeId) -> ComponentId> Entity<'a, F> {
+    pub(crate) fn new(component_mapper: F, mark_dirty: &'a mut FnMut()) -> Self {
         Entity {
             mask: BitSet::new(),
             components: FxHashMap::default(),
-            component_mapper
+            component_mapper: component_mapper,
+            mark_dirty: mark_dirty
         }
     }
 
@@ -23,10 +25,12 @@ impl<F: Fn(TypeId) -> ComponentId> Entity<F> {
     where T: Component {
         use std::collections::hash_map::Entry;
         let id = self.id_for::<T>();
+        let mark_dirty = &mut self.mark_dirty;
         let entry = self.components.entry(id);
 
         if let Entry::Vacant(e) = entry {
             e.insert(Box::new(component));
+            mark_dirty();
         } else if let Entry::Occupied(mut o) = entry {
             o.insert(Box::new(component));
         }
@@ -39,6 +43,8 @@ impl<F: Fn(TypeId) -> ComponentId> Entity<F> {
         let id = self.id_for::<T>();
         if (self.mask.contains(id as usize)) {
             self.mask.remove(id as usize);
+            let mark_dirty = &mut self.mark_dirty;
+            mark_dirty();
             let value = self.components.remove(&id).map(|b| b.downcast::<T>())
                 .expect("Failed to downcast component");
             return value.ok().map(|b| *b)
@@ -67,25 +73,29 @@ mod tests {
     struct C(u32);
 
     #[test]
-    pub fn add_component() {
-        let mut entity = Entity::new(|c| { 0 });
+    fn add_component() {
+        let mut mark_dirty = || {};
+        let mut entity = Entity::new(|c| { 0 }, &mut mark_dirty);
         let c = C(0);
+
 
         entity.add_component(c);
         assert!(entity.has_component::<C>());
     }
 
     #[test]
-    pub fn no_component() {
-        let mut entity = Entity::new(|c| { 0 });
+    fn no_component() {
+        let mut mark_dirty = || {};
+        let mut entity = Entity::new(|c| { 0 }, &mut mark_dirty);
         let c = C(0);
 
         assert!(!entity.has_component::<C>());
     }
 
     #[test]
-    pub fn remove_component() {
-        let mut entity = Entity::new(|c| { 0 });
+    fn remove_component() {
+        let mut mark_dirty = || {};
+        let mut entity = Entity::new(|c| { 0 }, &mut mark_dirty);
         let c = C(0);
 
         entity.add_component(c);
@@ -95,9 +105,38 @@ mod tests {
     }
 
     #[test]
-    pub fn remove_component_none() {
-        let mut entity = Entity::new(|c| { 0 });
+    fn remove_component_none() {
+        let mut mark_dirty = || {};
+        let mut entity = Entity::new(|c| { 0 }, &mut mark_dirty);
         let removed = entity.remove_component::<C>();
         assert!(removed.is_none());
     }
-}
+
+    #[test]
+    fn dirty_when_adding() {
+        let mut dirty = false;
+        {
+            let mut mark_dirty = || dirty = true;
+            let mut entity = Entity::new(|c| { 0 }, &mut mark_dirty);
+            let c = C(0);
+
+
+            entity.add_component(c);
+        }
+        assert!(dirty)
+    }
+
+    #[test]
+    fn dirty_when_removing() {
+        let mut dirty = 0;
+        {
+            let mut mark_dirty = || dirty += 1;
+            let mut entity = Entity::new(|c| { 0 }, &mut mark_dirty);
+            let c = C(0);
+
+            entity.add_component(c);
+            entity.remove_component::<C>();
+        }
+        assert_eq!(2, dirty);
+    }
+} 
