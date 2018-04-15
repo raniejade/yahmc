47287ed0460698a0e1eef8983aea4fc87e146c49
Ranie Jade Ramiso <raniejaderamiso@gmail.com>
2018-04-15 20:46:24 +1000

    Implement entity

--- ecs/src/entity.rs
+++ ecs/src/entity.rs
@@ -1,142 +1,89 @@
 use bit_set::BitSet;
 use fxhash::FxHashMap;
 use std::any::TypeId;
+use std::default::Default;
 
 use super::component::{Component, ComponentId};
 
-// pub struct Entity<'a, F: Fn(TypeId) -> ComponentId> {
-//     mask: BitSet,
-//     components: FxHashMap<ComponentId, Box<Component>>,
-//     component_mapper: F,
-//     mark_dirty: &'a mut FnMut()
-// }
+pub type Entity = usize;
 
-// impl<'a, F: Fn(TypeId) -> ComponentId> Entity<'a, F> {
-//     pub(crate) fn new(component_mapper: F, mark_dirty: &'a mut FnMut()) -> Self {
-//         Entity {
-//             mask: BitSet::new(),
-//             components: FxHashMap::default(),
-//             component_mapper: component_mapper,
-//             mark_dirty: mark_dirty
-//         }
-//     }
+#[derive(Derivative)]
+#[derivative(Default(bound = ""))]
+pub struct EntityStorage {
+    next_id: usize,
+    alive: BitSet,
+    limbo: Vec<usize>,
+}
 
-//     pub fn add_component<T>(&mut self, component: T) 
-//     where T: Component {
-//         use std::collections::hash_map::Entry;
-//         let id = self.id_for::<T>();
-//         let mark_dirty = &mut self.mark_dirty;
-//         let entry = self.components.entry(id);
+impl EntityStorage {
+    pub fn new() -> Self {
+        Default::default()
+    }
 
-//         if let Entry::Vacant(e) = entry {
-//             e.insert(Box::new(component));
-//             mark_dirty();
-//         } else if let Entry::Occupied(mut o) = entry {
-//             o.insert(Box::new(component));
-//         }
+    pub fn create(&mut self) -> Entity {
+        let id = if (!self.limbo.is_empty()) {
+            self.limbo.remove(0)
+        } else {
+            self.next_id()
+        };
 
-//         self.mask.insert(id as usize);
-//     }
+        self.alive.insert(id);
+        id as Entity
+    }
 
-//     pub fn remove_component<T>(&mut self) -> Option<T> 
-//     where T: Component {
-//         let id = self.id_for::<T>();
-//         if (self.mask.contains(id as usize)) {
-//             self.mask.remove(id as usize);
-//             let mark_dirty = &mut self.mark_dirty;
-//             mark_dirty();
-//             let value = self.components.remove(&id).map(|b| b.downcast::<T>())
-//                 .expect("Failed to downcast component");
-//             return value.ok().map(|b| *b)
-//         }
+    pub fn is_alive(&self, entity: Entity) -> bool {
+        self.alive.contains(entity)
+    }
 
-//         None
-//     }
+    pub fn destroy(&mut self, entity: Entity) {
+        assert!(self.is_alive(entity), "Can't destroy dead entity!");
+        self.limbo.push(entity);
+        self.alive.remove(entity);
+    }
 
-//     pub fn has_component<T>(&self) -> bool
-//     where T: Component {
-//         self.mask.contains(self.id_for::<T>() as usize)
-//     }
+    fn next_id(&mut self) -> usize {
+        let id = self.next_id;
+        self.next_id = id + 1;
+        id
+    }
+}
 
-//     fn id_for<T>(&self) -> ComponentId
-//     where T: Component {
-//         let mapper = &self.component_mapper;
-//         mapper(TypeId::of::<T>())
-//     }
-// }
+#[cfg(test)]
+mod tests {
+    use super::*;
+    
+    #[test]
+    fn create_entity() {
+        let mut entity_storage = EntityStorage::new();
+        let entity = entity_storage.create();
+        assert!(entity_storage.is_alive(entity));
+    }
 
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
+    #[test]
+    fn destroy_entity() {
+        let mut entity_storage = EntityStorage::new();
+        let entity = entity_storage.create();
+        entity_storage.destroy(entity);
+        assert!(!entity_storage.is_alive(entity));
+    }
+    
+    #[test]
+    #[should_panic]
+    fn destroy_entity_dead() {
+        let mut entity_storage = EntityStorage::new();
+        let entity = entity_storage.create();
+        entity_storage.destroy(entity);
+        // some time later
+        entity_storage.destroy(entity);
+    }
 
-//     #[derive(Component)]
-//     struct C(u32);
-
-//     #[test]
-//     fn add_component() {
-//         let mut mark_dirty = || {};
-//         let mut entity = Entity::new(|c| { 0 }, &mut mark_dirty);
-//         let c = C(0);
-
-
-//         entity.add_component(c);
-//         assert!(entity.has_component::<C>());
-//     }
-
-//     #[test]
-//     fn no_component() {
-//         let mut mark_dirty = || {};
-//         let mut entity = Entity::new(|c| { 0 }, &mut mark_dirty);
-//         let c = C(0);
-
-//         assert!(!entity.has_component::<C>());
-//     }
-
-//     #[test]
-//     fn remove_component() {
-//         let mut mark_dirty = || {};
-//         let mut entity = Entity::new(|c| { 0 }, &mut mark_dirty);
-//         let c = C(0);
-
-//         entity.add_component(c);
-//         let removed = entity.remove_component::<C>();
-//         assert!(removed.is_some());
-//         assert!(!entity.has_component::<C>());
-//     }
-
-//     #[test]
-//     fn remove_component_none() {
-//         let mut mark_dirty = || {};
-//         let mut entity = Entity::new(|c| { 0 }, &mut mark_dirty);
-//         let removed = entity.remove_component::<C>();
-//         assert!(removed.is_none());
-//     }
-
-//     #[test]
-//     fn dirty_when_adding() {
-//         let mut dirty = false;
-//         {
-//             let mut mark_dirty = || dirty = true;
-//             let mut entity = Entity::new(|c| { 0 }, &mut mark_dirty);
-//             let c = C(0);
-
-
-//             entity.add_component(c);
-//         }
-//         assert!(dirty)
-//     }
-
-//     #[test]
-//     fn dirty_when_removing() {
-//         let mut dirty = 0;
-//         {
-//             let mut mark_dirty = || dirty += 1;
-//             let mut entity = Entity::new(|c| { 0 }, &mut mark_dirty);
-//             let c = C(0);
-
-//             entity.add_component(c);
-//             entity.remove_component::<C>();
-//         }
-//         assert_eq!(2, dirty);
-//     }
-// } 
+    #[test]
+    fn re_use_dead_entity() {
+        let mut entity_storage = EntityStorage::new();
+        let entity = entity_storage.create();
+        entity_storage.destroy(entity);
+        
+        let new_entity = entity_storage.create();
+        assert_eq!(entity, new_entity);
+    }
+}
